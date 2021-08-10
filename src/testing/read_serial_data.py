fd6bdf388dd26f9f1df40cf95f60f8cd1be4525e
Michael Peeri <mich1@post.tau.ac.il>
2021-08-10 03:16:06 +0300

    Plot ADC data

--- src/testing/read_serial_data.py
+++ src/testing/read_serial_data.py
@@ -1,10 +1,17 @@
 import serial
-#import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 def analyze(d):
     for i in range(0, len(d), 8):
         p = d[i:i+8]
-        print(' '.join(map(str, map(ord, p))))
+        print(' '.join(map(str, map(int, p))))
+    
+    nd = np.array([n for i, n in enumerate(map(int, d)) if i%2==1])
+    fig, ax = plt.subplots()
+    plt.plot(nd)
+    plt.savefig('test.png')
+
 
 
 with serial.Serial('/dev/ttyUSB0', 115200, timeout=1) as ser:
